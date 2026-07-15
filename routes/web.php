7fc82355863b5c4b59a281497ddbcb877e27196f
Papa Ara <syahacbar@gmail.com>
2026-07-15 22:04:41 +0900

    fix: route landing page to login

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,9 @@ use App\Http\Controllers\SuperAdmin\UserController as SuperAdminUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return auth()->check()
+        ? redirect()->route('dashboard')
+        : redirect()->route('login');
 });
 
 Route::get('/dashboard', MahasiswaDashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
